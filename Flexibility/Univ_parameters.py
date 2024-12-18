9aa55d50073309e29c0de0dc5028ffa19e848cc9
Xinlin-CSIRO <118944229+xinlin-CSIRO@users.noreply.github.com>
2024-12-19 09:19:14 +1100

    Delete Flexibility directory
    Delete the un-related project here

--- Flexibility/Univ_parameters.py
+++
@@ -1,8 +0,0 @@
-
-def training_days():
-    _n_days_ = 600
-    return _n_days_
-
-def energy_consumption_up_boundary():
-    energy_consumption_up_bond = 4000
-    return(energy_consumption_up_bond)
